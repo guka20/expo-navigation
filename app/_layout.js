77d4e1936c01568a29c9f204642480e71abf5fc6
guka20 <gukadavitadze23@gmail.com>
2025-01-14 18:25:51 +0400

    project setup is done

---
+++ app/_layout.js
@@ -0,0 +1,21 @@
+import { StyleSheet, View, Text } from "react-native";
+
+export default function MainLayout() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 30,
+    color: "red",
+  },
+});
